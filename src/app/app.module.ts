50e90e79b18d4c4d8e279bc5f9994677a71ceb7a
Amani Ben Azzouz <amani.info.app@gmail.com>
2018-02-27 15:27:40 +0100

    refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,18 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavigationComponentComponent } from './navigation-component/navigation-component.component';
+import { SectionsComponentComponent } from './sections-component/sections-component.component';
+import { FooterComponentComponent } from './footer-component/footer-component.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponentComponent,
+    SectionsComponentComponent,
+    FooterComponentComponent
+    
   ],
   imports: [
     BrowserModule
